55d778b9a9a1c5b7ee9abee4868c4ecb235242de
MidnightCaffeine <jobert.simbre14@gmail.com>
2022-11-08 20:14:56 +0800

    rfid login logout

--- lib/process.php
+++ lib/process.php
@@ -1,26 +1,46 @@
-<?php 
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"])){
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["action"])) {
 	switch ($_POST['action']) {
 		case 'insertRecord':
-		insertRecord();
-		break;
+			insertRecord();
+			break;
 		case 'showProcess':
-		showProcess();
+			showProcess();
 		default:
-		break;
+			break;
 	}
 }
 
-function insertRecord() {
-    include 'connection.php';
-    $cardid = $_POST['cardid'];
-    $time = time();
-    $stmt = $pdo->prepare("INSERT INTO `rfid`(`cardid`, `logdate`) VALUES (:card, :dt)");
-    $stmt->bindParam(":card", $cardid);
-    $stmt->bindParam(":dt", $time);
-	$stmt->execute();
-	
-	echo "success";
+function insertRecord()
+{
+	include 'connection.php';
+	date_default_timezone_set('Asia/Manila');
+	$cardid = $_POST['cardid'];
+	$d = date("Y-m-d");
+	$t = date("H:i:sa");
+
+
+	$select = $pdo->prepare("SELECT * from user_list where card_number='$cardid'");
+	$select->execute();
+
+	while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+		$id = $row["id"];
+		if ($row["position"] == "Student") {
+
+			$select = $pdo->prepare("SELECT * from `student_list` where student_id = '$id'");
+			$select->execute();
+			while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+				$firstname = $row["student_firstname"];
+				$mname = $row["student_middlename"];
+				$lname = $row["student_lastname"];
+			}
+			$fullname = $firstname . " " . $mname . " " . $lname;
+			$insert = $pdo->prepare("INSERT INTO `attendance`(`name`, `date`, `time_in`) VALUES ($fullname, $d, $t)");
+			$insert->execute();
+
+			echo "success";
+		}
+	}
 }
 
 function showProcess()
@@ -28,14 +48,12 @@ function showProcess()
 	include 'connection.php';
 
 	$logs = $pdo->query("SELECT * FROM `rfid`");
-	while($r = $logs->fetch()){
+	while ($r = $logs->fetch()) {
 		echo "<tr>";
-		echo "<td>".$r['id']."</td>";
-		echo "<td>".$r['cardid']."</td>";
+		echo "<td>" . $r['id'] . "</td>";
+		echo "<td>" . $r['cardid'] . "</td>";
 		$dateadded = date("F j, Y, g:i a", $r["logdate"]);
-		echo "<td>".$dateadded."</td>";
+		echo "<td>" . $dateadded . "</td>";
 		echo "</tr>";
 	}
 }
-
-?>
